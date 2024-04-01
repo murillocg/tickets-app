e6b363f884cc1d575ed2a8dbd6bf0ee673a92710
Murillo Goulart <murillo.cg@gmail.com>
2024-04-01 17:21:30 -0300

    Added testcontainers to run integration tests with postgres and not the weird H2

--- src/test/java/com/murillocg/ticketapp/TicketApplicationTests.java
+++ src/test/java/com/murillocg/ticketapp/TicketApplicationTests.java
@@ -5,9 +5,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 public class TicketApplicationTests {
+
+	@Container
+	private static final PostgreSQLContainer<?> postgresContainer =
+			new PostgreSQLContainer<>("postgres:latest")
+					.withDatabaseName("ticketsdb")
+					.withUsername("postgres")
+					.withPassword("password");
 
 	@Autowired
 	private DispatcherServlet dispatcherServlet;
